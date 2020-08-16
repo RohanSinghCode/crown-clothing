c0a0d8c148717d34cb739abed4580a8c0e0b2a62
Rohan Singh <1andonly1rohan@gmail.com>
2020-08-16 23:37:49 +0530

    implemented firebase utils ability included ability to store

--- src/components/firebase/firebase.util.js
+++ src/components/firebase/firebase.util.js
@@ -12,7 +12,41 @@ const config = {
     messagingSenderId: "618636790193",
     appId: "1:618636790193:web:cd6c4b20def1d6b5b99590",
     measurementId: "G-605DBR6YBN"
-}
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if(!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`{users/${userAuth.ref}}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) 
+    {
+        const {displayName, email} = userAuth;
+
+        const createAt = new Date();
+
+        try 
+        {   
+                await userRef.set({
+                    displayName,
+                    email,
+                    createAt,
+                    ...additionalData
+                })
+        }catch(error) 
+        {
+                console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+};
 
 firebase.initializeApp(config);
 
